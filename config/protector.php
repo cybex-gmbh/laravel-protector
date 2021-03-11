04b66a791828712783b23c75d7b53159ce5eef42
Marco Szulik <marco.szulik@goodbabyint.com>
2021-03-11 15:45:26 +0100

    [style] (config) indent comment slashes

--- config/protector.php
+++ config/protector.php
@@ -55,7 +55,7 @@ return [
     |
     */
     'baseDirectory' => '',
-//    'diskName' => 'protector',
+    // 'diskName' => 'protector',
 
     /*
     |--------------------------------------------------------------------------
