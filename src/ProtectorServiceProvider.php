52e8665719e11350a1cc2dbc32dcf2dec1f68f35
Marco Szulik <marco.szulik@goodbabyint.com>
2021-08-03 15:32:51 +0200

    add option to publish the package migrations

--- src/ProtectorServiceProvider.php
+++ src/ProtectorServiceProvider.php
@@ -28,7 +28,10 @@ class ProtectorServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__ . '/../src/Migrations');
 
         // Publish package config to app config space.
-        $this->publishes([__DIR__ . '/../config/protector.php' => config_path('protector.php')]);
+        $this->publishes([__DIR__ . '/../config/protector.php' => config_path('protector.php')
+        ], 'protector.config');
+
+        $this->publishMigrations();
     }
 
     /**
@@ -53,5 +56,24 @@ class ProtectorServiceProvider extends ServiceProvider
             'as'   => 'protectorDumpEndpointRoute',
             'uses' => Protector::class . '@generateFileDownloadResponse',
         ])->middleware(config('protector.routeMiddleware'));
+    }
+
+    /**
+     * Publish the package's migrations.
+     *
+     * @return void
+     */
+    protected function publishMigrations()
+    {
+        if (class_exists('AddPublicKeyToUsersTable')) {
+            return;
+        }
+
+        $timestamp = date('Y_m_d_His', time());
+
+        $stub = sprintf('%s/Migrations/2020_08_26_073644_add_public_key_to_users_table.php', __DIR__);
+        $target = sprintf('%s/migrations/%s_add_public_key_to_users_table.php', $this->app->databasePath(), $timestamp);
+
+        $this->publishes([$stub => $target], 'protector.migrations');
     }
 }
