7639042f01fe2da47ce1612a482fd9c81a869ef3
Fabian Holy <fabian.holy@cybex-online.com>
2022-06-29 08:20:19 +0200

    add a custom laravel scaffold for testing.

--- Tests/BaseTest.php
+++ Tests/BaseTest.php
@@ -1,16 +1,42 @@
 <?php
 
-namespace Cybex\Protector\Tests;
 
 use Cybex\Protector\ProtectorServiceProvider;
-use Orchestra\Testbench\TestCase;
+use Illuminate\Support\Facades\Storage;
+use Orchestra\Testbench\TestCase as Orchestra;
 
-abstract class BaseTest extends TestCase
+abstract class BaseTest extends Orchestra
 {
+    /**
+     * @return string
+     */
+    protected function getBasePath(): string
+    {
+        return __DIR__ . '/scaffolds/includes-protector';
+    }
+
+    /**
+     * @param $app
+     * @return string[]
+     */
     protected function getPackageProviders($app): array
     {
         return [
             ProtectorServiceProvider::class,
         ];
+    }
+
+    /**
+     * @param $app
+     * @return void
+     */
+    protected function defineEnvironment($app): void
+    {
+        Storage::disk('local')->put('protector/dump.sql', '');
+    }
+
+    protected function usesMultipleDumps()
+    {
+        Storage::disk('local')->put('protector/secondDump.sql', '');
     }
 }
