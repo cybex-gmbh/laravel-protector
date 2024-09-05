97fc029b308c5b8fb8c80e5f58e92802ec94083f
Oliver Matla <info@lupinitylabs.com>
2024-09-05 02:20:06 +0200

    apply code style changes

--- src/Protector.php
+++ src/Protector.php
@@ -3,8 +3,10 @@
 namespace Cybex\Protector;
 
 use Cybex\Protector\Classes\MySqlSchemaStateProxy;
+use Cybex\Protector\Classes\PostgresSchemaStateProxy;
 use Cybex\Protector\Exceptions\FailedCreatingDestinationPathException;
 use Cybex\Protector\Exceptions\FailedDumpGenerationException;
+use Cybex\Protector\Exceptions\FailedImportException;
 use Cybex\Protector\Exceptions\FailedMysqlCommandException;
 use Cybex\Protector\Exceptions\FailedRemoteDatabaseFetchingException;
 use Cybex\Protector\Exceptions\FileNotFoundException;
@@ -19,6 +21,7 @@ use GuzzleHttp\Psr7\StreamWrapper;
 use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Database\Connection;
 use Illuminate\Database\Schema\MySqlSchemaState;
+use Illuminate\Database\Schema\PostgresSchemaState;
 use Illuminate\Database\Schema\SchemaState;
 use Illuminate\Filesystem\FilesystemAdapter;
 use Illuminate\Http\Client\PendingRequest;
@@ -46,17 +49,15 @@ class Protector
 
     /**
      * Cache for the runtime-metadata for a new dump.
-     *
-     * @var array
      */
     protected array $metaDataCache = [];
 
-    public function __construct(string $connectionName = null)
+    public function __construct(?string $connectionName = null)
     {
         $this->withConnectionName($connectionName)
-             ->withDefaultMaxPacketLength()
-             ->withoutCreateDb()
-             ->withoutTablespaces();
+            ->withDefaultMaxPacketLength()
+            ->withoutCreateDb()
+            ->withoutTablespaces();
     }
 
     /**
@@ -67,6 +68,7 @@ class Protector
      * @throws InvalidConnectionException
      * @throws FileNotFoundException
      * @throws InvalidConfigurationException
+     * @throws FailedImportException
      */
     public function importDump(string $sourceFilePath, array $options = []): void
     {
@@ -216,8 +218,8 @@ class Protector
         $request = $this->getConfiguredHttpRequest();
 
         if ($isTelescopeRecording = class_exists(
-                \Laravel\Telescope\Telescope::class
-            ) && \Laravel\Telescope\Telescope::isRecording()) {
+            \Laravel\Telescope\Telescope::class
+        ) && \Laravel\Telescope\Telescope::isRecording()) {
             \Laravel\Telescope\Telescope::stopRecording();
         }
 
@@ -237,15 +239,15 @@ class Protector
             $httpCode = $response->status();
 
             throw match ($httpCode) {
-                401, 403 => new UnauthorizedHttpException('', $httpCode.' Unauthorized access'),
-                404 => new NotFoundHttpException('404 Not found: '.$serverUrl),
+                401, 403 => new UnauthorizedHttpException('', $httpCode . ' Unauthorized access'),
+                404 => new NotFoundHttpException('404 Not found: ' . $serverUrl),
                 500 => new FailedRemoteDatabaseFetchingException($response->header('message')),
-                default => new HttpException($httpCode, 'Status code '.$httpCode),
+                default => new HttpException($httpCode, 'Status code ' . $httpCode),
             };
         }
 
         $destinationFilePath = $this->getDumpDestinationFilePath($response->header('Content-Disposition'));
-        $stream              = $response->toPsrResponse()->getBody();
+        $stream = $response->toPsrResponse()->getBody();
 
         $this->writeDumpFile(
             $stream,
@@ -308,10 +310,10 @@ class Protector
         }
 
         /** @var Connection $connection */
-        $connection       = DB::connection($this->connectionName);
-        $schemaState      = $connection->getSchemaState();
+        $connection = DB::connection($this->connectionName);
+        $schemaState = $connection->getSchemaState();
         $schemaStateProxy = $this->getProxyForSchemaState($schemaState);
-        $tempFile         = tempnam('', 'protector');
+        $tempFile = tempnam('', 'protector');
 
         $schemaStateProxy->dump($connection, $tempFile);
 
@@ -381,19 +383,19 @@ class Protector
             $this->guardExecEnabled();
 
             $gitInformation = [
-                'gitRevision'     => $this->getGitRevision(),
-                'gitBranch'       => $this->getGitBranch(),
+                'gitRevision' => $this->getGitRevision(),
+                'gitBranch' => $this->getGitBranch(),
                 'gitRevisionDate' => $this->getGitHeadDate(),
             ];
         }
 
         return $this->metaDataCache = [
-            'database'        => $this->connectionConfig['database'],
-            'connection'      => $this->connectionName,
-            'gitRevision'     => $gitInformation['gitRevision'] ?? null,
-            'gitBranch'       => $gitInformation['gitBranch'] ?? null,
+            'database' => $this->connectionConfig['database'],
+            'connection' => $this->connectionName,
+            'gitRevision' => $gitInformation['gitRevision'] ?? null,
+            'gitBranch' => $gitInformation['gitBranch'] ?? null,
             'gitRevisionDate' => $gitInformation['gitRevisionDate'] ?? null,
-            'dumpedAtDate'    => now(),
+            'dumpedAtDate' => now(),
         ];
     }
 
@@ -415,7 +417,7 @@ class Protector
         fseek($fileHandle, 0, SEEK_END);
 
         $linesToRead = $lines;
-        $contents    = '';
+        $contents = '';
 
         // Only read file as long as file-pointer is not at start of the file and there are still lines to read open.
         while (ftell($fileHandle) && $linesToRead >= 0) {
@@ -426,7 +428,7 @@ class Protector
             fseek($fileHandle, -$seekLength, SEEK_CUR);
 
             // Get the next content-chunk by using the according length.
-            $contents = ($chunk = fread($fileHandle, $seekLength)).$contents;
+            $contents = ($chunk = fread($fileHandle, $seekLength)) . $contents;
 
             // Reset pointer to the position before reading the current chunk.
             fseek($fileHandle, -mb_strlen($chunk, 'UTF-8'), SEEK_CUR);
@@ -471,7 +473,7 @@ class Protector
      */
     public function generateFileDownloadResponse(
         Request $request,
-        string $connectionName = null
+        ?string $connectionName = null
     ): Response|StreamedResponse {
         $shouldEncrypt = $this->shouldEncrypt();
 
@@ -480,7 +482,7 @@ class Protector
             if ($this->withConnectionName($connectionName)) {
                 try {
                     $serverFilePath = $this->createDump();
-                    $publicKey      = $this->getPublicKey($request);
+                    $publicKey = $this->getPublicKey($request);
                 } catch (InvalidConnectionException|FailedDumpGenerationException|InvalidConfigurationException $exception) {
                     return response($exception->getMessage(), 500, ['message' => $exception->getMessage()]);
                 }
@@ -488,7 +490,7 @@ class Protector
                 $chunkSize = $this->getConfigValueForKey('chunkSize');
 
                 return response()->streamDownload(
-                    function () use ($publicKey, $request, $serverFilePath, $chunkSize, $shouldEncrypt) {
+                    function () use ($publicKey, $serverFilePath, $chunkSize, $shouldEncrypt) {
                         $inputHandle = fopen($serverFilePath, 'rb');
 
                         while (!feof($inputHandle)) {
@@ -507,14 +509,14 @@ class Protector
                     },
                     $this->createFilename(),
                     [
-                        'Content-Type'    => 'text/plain',
-                        'Pragma'          => 'no-cache',
-                        'Expires'         => gmdate(DATE_RFC7231, time() - 3600),
+                        'Content-Type' => 'text/plain',
+                        'Pragma' => 'no-cache',
+                        'Expires' => gmdate(DATE_RFC7231, time() - 3600),
                         // Encryption adds some overhead to the chunk, which has to be considered when decrypting it.
-                        'Chunk-Size'      => $shouldEncrypt ? $chunkSize + $this->determineEncryptionOverhead(
-                                $chunkSize,
-                                $publicKey
-                            ) : $chunkSize,
+                        'Chunk-Size' => $shouldEncrypt ? $chunkSize + $this->determineEncryptionOverhead(
+                            $chunkSize,
+                            $publicKey
+                        ) : $chunkSize,
                         'Sanctum-Enabled' => $shouldEncrypt,
                     ]
                 );
@@ -570,7 +572,7 @@ class Protector
         } elseif ($htaccessLogin) {
             // Add basic authentication to request.
             $credentials = explode(':', $htaccessLogin);
-            $request     = Http::withBasicAuth($credentials[0], $credentials[1]);
+            $request = Http::withBasicAuth($credentials[0], $credentials[1]);
         } else {
             // Protector cannot be used without any authentication.
             throw new InvalidConfigurationException(
@@ -588,9 +590,9 @@ class Protector
      */
     public function getLatestDumpName(): string
     {
-        $disk          = $this->getDisk();
+        $disk = $this->getDisk();
         $baseDirectory = $this->getConfigValueForKey('baseDirectory');
-        $files         = $disk->files($baseDirectory);
+        $files = $disk->files($baseDirectory);
 
         if (empty($files)) {
             throw new FileNotFoundException($disk->path($baseDirectory));
@@ -612,7 +614,7 @@ class Protector
 
         if ($decryptedString === false) {
             throw new InvalidConfigurationException(
-                "There was an error decrypting the provided string. This might be due to mismatching crypto keys."
+                'There was an error decrypting the provided string. This might be due to mismatching crypto keys.'
             );
         }
 
@@ -643,19 +645,19 @@ class Protector
      */
     public function createTempFilePath(string $diskFilePath): string
     {
-        $tempFilePath = tempnam('', 'protector');
+        $tempFilePath = tempnam('', 'protector') . '.sql';
 
         if ($tempFilePath === false) {
             throw new Exception('Could not create a temporary file for dump.');
         }
 
-        $handle       = fopen($tempFilePath, 'w');
+        $handle = fopen($tempFilePath, 'w');
 
         if ($handle === false) {
             throw new Exception('Could not open temporary file for writing.');
         }
 
-        $stream       = $this->getDisk()->readStream($diskFilePath);
+        $stream = $this->getDisk()->readStream($diskFilePath);
 
         if (!is_resource($stream)) {
             fclose($handle);
@@ -697,7 +699,7 @@ class Protector
     public function guardExecEnabled(): void
     {
         if (!function_exists('exec')) {
-            throw new ShellAccessDeniedException();
+            throw new ShellAccessDeniedException;
         }
     }
 
@@ -747,7 +749,7 @@ class Protector
 
     protected function determineEncryptionOverhead(int $chunkSize, string $publicKey): int
     {
-        $chunk          = str_repeat('0', $chunkSize);
+        $chunk = str_repeat('0', $chunkSize);
         $encryptedChunk = sodium_crypto_box_seal($chunk, $publicKey);
 
         return strlen($encryptedChunk) - $chunkSize;
@@ -760,9 +762,9 @@ class Protector
     {
         return match (get_class($schemaState)) {
             MySqlSchemaState::class => app(MySqlSchemaStateProxy::class, [$schemaState, $this]),
-//            PostgresSchemaState::class => app('PostgresSchemaStateProxy', [$schemaState, $this]),
-//            SqliteSchemaState::class => app('SqliteSchemaStateProxy', [$schemaState, $this]),
-            default => throw new UnsupportedDatabaseException('Unsupported database schema state: '.class_basename($schemaState)),
+            PostgresSchemaState::class => app(PostgresSchemaStateProxy::class, [$schemaState, $this]),
+            //            SqliteSchemaState::class => app('SqliteSchemaStateProxy', [$schemaState, $this]),
+            default => throw new UnsupportedDatabaseException('Unsupported database schema state: ' . class_basename($schemaState)),
         };
     }
 }
