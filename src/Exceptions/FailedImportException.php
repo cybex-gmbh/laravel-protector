5da5ed8c21ee5deb04936dfc3b6bb6714750439e
Oliver Matla <info@lupinitylabs.com>
2024-09-05 02:16:34 +0200

    add new exception for failed imports through SchemaState `load()`

---
+++ src/Exceptions/FailedImportException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Cybex\Protector\Exceptions;
+
+use Exception;
+
+/**
+ * Class FailedShellCommandException
+ *
+ * Thrown if a shell command couldn't be executed properly.
+ */
+class FailedImportException extends Exception
+{
+}
