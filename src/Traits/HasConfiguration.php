fc6c2806f80e31ca573ee79c8e725d046e23d0a3
Marco Szulik <marco.szulik@cybex-online.com>
2025-04-08 10:01:23 +0200

    add the max packet length to the metadata at the end of a dump file

--- src/Traits/HasConfiguration.php
+++ src/Traits/HasConfiguration.php
@@ -286,6 +286,12 @@ trait HasConfiguration
         return $this->connectionConfig['database'];
     }
 
+    /**
+     * Returns the maximum packet length specified in the config.
+     * The option 'max_allowed_packet' sets an upper limit on the size of any single message between the MySQL server and clients.
+     * This has to be set up on the client (here) and the MySQL server.
+     * This is not applicable to PostgreSQL.
+     */
     public function getMaxPacketLength(): string
     {
         return $this->maxPacketLength;
