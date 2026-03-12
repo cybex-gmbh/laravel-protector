e6099c8617a81b4fa032f7a4835e39985d8cdea9
Marco Szulik <marco.szulik@cybex-online.com>
2026-03-12 16:36:29 +0100

    remove unnecessary string checks in test

--- tests/Feature/CreateKeysCommandTest.php
+++ tests/Feature/CreateKeysCommandTest.php
@@ -7,10 +7,6 @@ use Cybex\Protector\Tests\TestCase;
 
 class CreateKeysCommandTest extends TestCase
 {
-    const SUCCESS_MESSAGE = 'Successfully generated crypto key pair.';
-    const SEND_TO_ADMIN = 'Send the public key to server admin so that it can be stored.';
-    const ADD_TO_ENV = 'Add the private key to your .env file.';
-    const PROTECTOR_PUBLIC_KEY_COMMENT = '# Protector Public Key:';
     const PRIVATE_KEY = 'MOCKED_PRIVATE_KEY';
     const PUBLIC_KEY = 'MOCKED_PUBLIC_KEY_FROM_PRIVATE_KEY';
 
@@ -22,14 +18,9 @@ class CreateKeysCommandTest extends TestCase
         CrypterFacade::shouldReceive('createPrivateKey')->andReturn(static::PRIVATE_KEY);
         CrypterFacade::shouldReceive('getPublicKeyFromPrivateKey')->andReturn(static::PUBLIC_KEY);
 
-        $privateKeyName = $this->protector->getPrivateKeyName();
-
         $this->artisan('protector:keys')
-            ->expectsOutput(static::SUCCESS_MESSAGE)
-            ->expectsOutput(static::SEND_TO_ADMIN)
-            ->expectsOutput(static::ADD_TO_ENV)
-            ->expectsOutput(sprintf('%s %s', static::PROTECTOR_PUBLIC_KEY_COMMENT, static::PUBLIC_KEY))
-            ->expectsOutput(sprintf('%s=%s', $privateKeyName, static::PRIVATE_KEY))
+            ->expectsOutputToContain(static::PUBLIC_KEY)
+            ->expectsOutputToContain(static::PRIVATE_KEY)
             ->assertExitCode(0);
     }
 }
