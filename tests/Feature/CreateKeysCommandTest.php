936729187ff50345527d97acf591bc88ff07ea95
Marco Szulik <marco.szulik@cybex-online.com>
2026-03-12 11:25:16 +0100

    improve create keys command test

--- tests/Feature/CreateKeysCommandTest.php
+++ tests/Feature/CreateKeysCommandTest.php
@@ -2,6 +2,7 @@
 
 namespace Cybex\Protector\Tests\Feature;
 
+use Cybex\Protector\Facades\CrypterFacade;
 use Cybex\Protector\Tests\TestCase;
 
 class CreateKeysCommandTest extends TestCase
@@ -9,21 +10,26 @@ class CreateKeysCommandTest extends TestCase
     const SUCCESS_MESSAGE = 'Successfully generated crypto key pair.';
     const SEND_TO_ADMIN = 'Send the public key to server admin so that it can be stored.';
     const ADD_TO_ENV = 'Add the private key to your .env file.';
-    const PROTECTOR_PUBLIC_KEY_COMMENT = '# Protector Public Key: ';
+    const PROTECTOR_PUBLIC_KEY_COMMENT = '# Protector Public Key:';
+    const PRIVATE_KEY = 'MOCKED_PRIVATE_KEY';
+    const PUBLIC_KEY = 'MOCKED_PUBLIC_KEY_FROM_PRIVATE_KEY';
 
     /**
      * @test
      */
     public function ensureCorrectOutput()
     {
+        CrypterFacade::shouldReceive('createPrivateKey')->andReturn(static::PRIVATE_KEY);
+        CrypterFacade::shouldReceive('getPublicKeyFromPrivateKey')->andReturn(static::PUBLIC_KEY);
+
         $privateKeyName = $this->protector->getPrivateKeyName();
 
         $this->artisan('protector:keys')
             ->expectsOutput(static::SUCCESS_MESSAGE)
             ->expectsOutput(static::SEND_TO_ADMIN)
             ->expectsOutput(static::ADD_TO_ENV)
-            ->expectsOutputToContain(static::PROTECTOR_PUBLIC_KEY_COMMENT)
-            ->expectsOutputToContain(sprintf('%s=', $privateKeyName))
+            ->expectsOutput(sprintf('%s %s', static::PROTECTOR_PUBLIC_KEY_COMMENT, static::PUBLIC_KEY))
+            ->expectsOutput(sprintf('%s=%s', $privateKeyName, static::PRIVATE_KEY))
             ->assertExitCode(0);
     }
 }
